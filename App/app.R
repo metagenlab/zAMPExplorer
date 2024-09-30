1c80adc52876ad0595079b53b59927bd674c3918
Sedreh Nassirnia <43466727+Sedreh@users.noreply.github.com>
2024-09-30 13:48:02 +0200

    Add files via upload

---
+++ App/app.R
@@ -0,0 +1,12 @@
+
+Sys.setenv(CHROMOTE_CHROME = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
+
+source("ui.R")
+source("server.R")
+
+# Create the Shiny app
+shinyApp(ui = ui, server = server)
+
+
+
+
