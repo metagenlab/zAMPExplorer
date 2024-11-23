5d35dc49be47a8def8e1b9f70890430d00aa344b
Sedreh Nassirnia <43466727+Sedreh@users.noreply.github.com>
2024-11-23 17:17:47 +0100

    Delete R/zAMPExplorer_app.R

--- R/zAMPExplorer_app.R
+++
@@ -1,7 +0,0 @@
-#' @title Launch the Shiny App
-#' @description This function launches the Shiny app and installs any missing dependencies.
-#' @export
-zAMPExplorer <- function() {
-  library(shiny)
-  shinyApp(ui = ui, server = server)
-}
