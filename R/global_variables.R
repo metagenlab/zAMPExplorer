308ef6a063b4fd0674d73218c7ba16aa680ccd89
Sedreh Nassirnia <43466727+Sedreh@users.noreply.github.com>
2024-12-20 07:22:54 +0100

    Delete R directory

--- R/global_variables.R
+++
@@ -1,8 +0,0 @@
-# This file declares global variables to suppress R CMD check warnings
-# about undefined global variables in the package code.
-
-utils::globalVariables(c(
-  "Reads", "Group", "Sample", "CAP1", "CAP2",
-  "subplot", "value", "Taxa", "Abundance",
-  "Prevalence", "Genus", "Phylum", "qval", "ASV"
-))
